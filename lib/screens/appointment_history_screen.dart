317ee21dbf098bf936ef5b68886a66b9a76a9a9f
MahRukh <mahrukhdev@gmail.com>
2025-06-11 22:36:10 +0500

    appointment screens

---
+++ lib/screens/appointment_history_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppointmentHistoryScreen extends StatefulWidget {
+  const AppointmentHistoryScreen({super.key});
+
+  @override
+  State<AppointmentHistoryScreen> createState() => _AppointmentHistoryScreenState();
+}
+
+class _AppointmentHistoryScreenState extends State<AppointmentHistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
