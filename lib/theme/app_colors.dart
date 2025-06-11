ad7d5cd4985393461adf19474313356be6b37a75
MahRukh <mahrukhdev@gmail.com>
2025-06-11 20:52:34 +0500

    theme setup

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.teal;
+  static const Color secondaryColor = Colors.blueAccent;
+  static const Color backgroundColor = Colors.white;
+  static const Color textColor = Colors.black87;
+}
