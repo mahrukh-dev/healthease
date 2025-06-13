e7fa03b59410b79499208055bdfcfce7e871b947
MahRukh <mahrukhdev@gmail.com>
2025-06-13 20:29:32 +0500

    changes

---
+++ lib/screens/appointment_confirmation_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:healthease/models/appointment_model.dart';
+
+class AppointmentConfirmationScreen extends StatefulWidget {
+  const AppointmentConfirmationScreen({super.key, required AppointmentModel appointment});
+
+  @override
+  State<AppointmentConfirmationScreen> createState() => _AppointmentConfirmationScreenState();
+}
+
+class _AppointmentConfirmationScreenState extends State<AppointmentConfirmationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
